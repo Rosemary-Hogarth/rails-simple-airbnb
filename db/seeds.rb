d79b16d8c4b9b0e455f06c4665649ebf7ebb04af
Rosemary Hogarth <rohogarth@gmail.com>
2024-05-17 15:01:36 +0200

    First user stories done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Roomy Flat London',
+  address: '20 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 90,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Wake up in the Musée d’Orsay',
+  address: '20 Rue Noir Paris W9',
+  description: 'A lovely cozy feel',
+  price_per_night: 100,
+  number_of_guests: 1
+)
+
+Flat.create!(
+  name: 'Amazing villa with pool just for you',
+  address: 'Heringsdorf, Germany W9',
+  description: 'In good weather, the sun terrace, the breakfast terrace and the large garden invite you to relax.',
+  price_per_night: 100,
+  number_of_guests: 1
+)
+
+puts 'Database seeded successfully!'
